49205999c7c1b757bb7cb4631e77a9aa9b50073b
dorameme <doradorav@naver.com>
2023-09-30 15:13:30 +0900

    유틸리티 객체와 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -51,6 +52,11 @@ public class BasicController {
     public String basicObjects(HttpSession session){
         session.setAttribute("sessionDate", "hello session");
         return "basic/basic-objects";
+    }
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("LocalDateTime", LocalDateTime.now());
+        return "basic/date";
     }
     @Component("helloBean")
     static class HelloBean{
